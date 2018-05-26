5ca396a39ef60765aa221c4d63cfae62019f0e11
bryanhuntesl <bryan.hunt@erlang-solutions.com>
2018-05-26 13:09:40 +0100

    close to working - just eleveldb missing as startup dep

--- mix.exs
+++ mix.exs
@@ -29,16 +29,21 @@ defmodule NoSlides.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:riak_core, git: "git@github.com:marianoguerra/riak_core.git", branch: "riak_core_lite_ng"},
-      {:eleveldb,  [ git: "git@github.com:freeakdb/eleveldb.git", tag: "2.2.20", manager: :rebar3, override: true]},
+      {:folsom,  [ git: "git@github.com:freeakdb/folsom.git", tag: "0.8.2p1", manager: :rebar3, override: true]},
+      {:riak_core, git: "git@github.com:freeakdb/riak_core.git", branch: "riak_core_lite_ng"},
+      {:bear,  [ git: "git@github.com:freeakdb/bear.git", tag: "0.8.2p1-rebar3-otp20", manager: :rebar3, override: true]}, 
+      {:eleveldb,  [ git: "git@github.com:freeakdb/eleveldb.git", tag: "2.2.19", manager: :rebar3, override: true]},
       {:lager,  [ git: "git://github.com/erlang-lager/lager", tag: "3.5.2", manager: :rebar3, override: true]},
-      {:goldrush, "~>0.1.8", [env: :prod, repo: "hexpm", hex: "goldrush", manager: :rebar3, override: true]},
+      {:goldrush,  [ git: "git@github.com:freeakdb/goldrush.git", tag: "0.1.9",manager: :rebar3,  override: true]},
       {:cuttlefish,  [ git: "git://github.com/bryanhuntesl/cuttlefish.git", branch: "develop",  override: true]},
       {:getopt,  [ git: "https://github.com/freeakdb/getopt.git", tag: "r20-rebar3", manager: :rebar, override: true ]},
       {:gen_fsm_compat,  [ git: "https://github.com/freeakdb/gen_fsm_compat.git", branch: "elixir1.6-r20", manager: :rebar, override: true ]},
       {:clique,  [ git: "https://github.com/freeakdb/clique.git", branch: "develop-2.2", manager: :rebar, override: true ]},
       {:riak_sysmon,  [ git: "https://github.com/freeakdb/riak_sysmon.git", branch: "develop-2.2", manager: :rebar, override: true ]},
-      {:riak_ensemble,  [ git: "https://github.com/freeakdb/riak_ensemble.git", branch: "develop-2.2", manager: :rebar, override: true ]},
+      {:riak_ensemble,  [ git: "https://github.com/freeakdb/riak_ensemble.git", tag: "2.1.9-rebar3-otp20-elixir-1.6", manager: :rebar, override: true ]},
+
+
+
     ]
   end
 end
