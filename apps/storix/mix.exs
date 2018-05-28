77768fe79092aa9c3c32d947230e6f54d063595d
bryanhuntesl <bryan.hunt@erlang-solutions.com>
2018-05-28 22:35:01 +0100

    latest

--- apps/storix/mix.exs
+++ apps/storix/mix.exs
@@ -18,6 +18,13 @@ defmodule Storix.Mixfile do
 
   defp deps do
     [
+      {:goldrush, [ git: "git@github.com:freeakdb/goldrush.git", tag: "0.1.9",manager: :rebar3, override: true]},
+      {:lager,  [ git: "git://github.com/erlang-lager/lager", tag: "3.5.2", manager: :rebar3, override: true]},
+      {:gen_state_machine    , "~> 2.0"   } ,
+
+      {:leveled, [git: "https://github.com/martinsumner/leveled.git", branch: "master"]},
+
+
       {:folsom,  [ git: "git@github.com:freeakdb/folsom.git", tag: "0.8.2p1", manager: :rebar3, override: true]},
       {:riak_core, git: "git@github.com:freeakdb/riak_core.git", branch: "riak_core_lite_training_bh"},
       {:bear,  [ git: "git@github.com:freeakdb/bear.git", tag: "0.8.2p1-rebar3-otp20", manager: :rebar3, override: true]},
